3c2a361ee42c525a520bcd2ff09724501f75dbc1
Rahmatsyah Yusuf <71863166+yusufyop@users.noreply.github.com>
2026-07-12 14:46:42 +0700

    Update server.js

--- backend-klinik/server.js
+++ backend-klinik/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
-const cors = require('cors');
+// const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
